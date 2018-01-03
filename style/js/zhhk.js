db5137de4d7fa31dcb88cb627f6b5febd8622060
Wish_Time <wish-time@outlook.com>
2018-01-03 14:26:49 +0800

    Initial commit

---
+++ style/js/zhhk.js
@@ -0,0 +1,32 @@
+﻿$(function(){
+	nav_li=$('#nav>li');
+	nav_li.on('hover',function(){
+		$(this).children('a:first').toggleClass('active').siblings('ul').stop(true).slideToggle('fast');
+	});
+	
+	short_btn=$('.short_msg .switch');
+	short_btn.on('click',function(){
+		pr=$(this).parent();
+		prw=pr.width();
+		if(pr.css('left')<'0px')
+		{
+			pr.css('left','0px');
+			$(this).css('background-position','0 0');
+		}
+		else
+		{
+			pr.css('left',-prw);
+			$(this).css('background-position','-29px 0');
+		}
+		
+	});
+	
+	var ansli=$('#aside .news .switch_bar li');
+	ac='active'
+	ansli.on('mouseover',function(){
+		i=$(this).index();
+		ansli.removeClass(ac).eq(i).addClass(ac).parent().siblings('ul').hide().eq(i).show();
+	});
+	
+	
+});
